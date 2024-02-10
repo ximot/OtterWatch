57843385632b1f4f0b9ae7f68bf70aa07d44075b
Tomasz Wyderka <tomaszwyderka@me.com>
2024-02-10 23:45:51 +0100

    Added licence file Added logo :)

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,8 +1,16 @@
 use crate::Settings;
-use std::fs;
+use config::{Config, File};
 
-fn load_config() -> Settings {
-    let config_file = fs::read_to_string("Settings.toml").expect("Error loading config file");
-    let config: Settings = toml::from_str(&config_file).expect("Wrong config file format!");
-    config
+// fn load_config() -> Settings {
+//     let config_file = fs::read_to_string("settings.toml").expect("Error loading config file");
+//     let config: Settings = toml::from_str(&config_file).expect("Wrong config file format!");
+//     config
+// }
+
+pub fn load_confg() -> rusqlite::Result<Settings, config::ConfigError> {
+    let mut settings = Config::default();
+    settings
+        .merge(File::with_name("settings"))?
+        .merge(config::Environment::with_prefix("APP"))?;
+    settings.try_into()
 }
